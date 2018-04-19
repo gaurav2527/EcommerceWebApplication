3d5fdcbf483b76c4bd8900f7e32810d50ae9d101
gaurav2527 <gaurav.sharma@magnatech.in>
2018-04-19 17:34:19 +0530

    Add project files.

---
+++ EcommerceWebApplication/Controllers/CustomersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceWebApplication.Models.EF;
+
+namespace EcommerceWebApplication.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ECommerce db = new ECommerce();
+        private Customer customer = new Customer();
+
+        //public Customer Customer1 { get => Customer; set => Customer = value; }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+       
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerName,State,Address,ContactNumber,email,CustomerPassword")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //customer.CustomerID++; ;
+               // db.ExecuteCommand("SET IDENTITY_INSERT MyTable ON");
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                //id = customer.CustomerID;
+                //id++;
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+
+    public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "Email,CustomerPassword")]UserLogin user)
+        {
+            if (ModelState.IsValid)
+                
+                {
+                    var obj = db.Customers.Where(a => a.email.Equals(user.email) && a.CustomerPassword.Equals(user.CustomerPassword)).FirstOrDefault();
+                    if(obj!=null)
+                {
+                    Session["CustomerID"] = obj.CustomerID.ToString();
+                    Session["email"] = obj.email.ToString();
+                    return RedirectToAction("Welcome");
+                }
+                }
+            return View("Login");
+        }
+
+        public ActionResult Welcome()
+        {
+            if(Session["CustomerID"]!=null)
+            {
+                return View();
+            }
+
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            
+        }
+     
+        // POST: Customers/Delete/5
+    
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ EcommerceWebApplication/Controllers/ProductInfoesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceWebApplication.Models.EF;
+
+namespace EcommerceWebApplication.Controllers
+{
+    public class ProductInfoesController : Controller
+    {
+        private ECommerce db = new ECommerce();
+        private ProductInfo pro = new ProductInfo();
+
+       
+
+        // GET: ProductInfoes
+        public ActionResult Details()
+        {
+            
+                var productInfoes = db.ProductInfoes.Include(p => p.ProductCategory);
+                
+            return View(productInfoes.ToList());
+            /*return View(from pro in db.ProductInfoes.Take(1)
+                        select pro);*/
+        }
+
+
+    }
+}
